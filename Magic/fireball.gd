b941ad4923b9e815745dd6b128c5080c04869e6a
Jose Ruiz <jruiz170.business@gmail.com>
2023-09-21 01:06:36 -0400

    Changed collision layers and masks for various objects. Added functionality for damage with fireball.

--- Magic/fireball.gd
+++ Magic/fireball.gd
@@ -16,4 +16,15 @@ func _process(delta):
 
 
 func _on_unqueue_timer_timeout():
+	print("Your fireball faded away")
 	queue_free()
+
+
+func _on_area_3d_body_entered(body):
+	if "Enemy" in body.name:
+		print("Your fireball struck an enemy!")
+		body.takeDamage(5)
+		queue_free()
+	else:
+		print("Your fireball did not hit the enemy. It hit the ", body.name)
+		queue_free()
